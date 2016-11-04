67e15ff707990cc21a81988a4c1127aab6ab8d1b
chuan <364608910@qq.com>
2016-11-04 11:14:56 +0800

    优化pic choose

--- chuanlibrary/src/main/java/com/husky/library/photo/photo/MediaChoseActivity.java
+++ chuanlibrary/src/main/java/com/husky/library/photo/photo/MediaChoseActivity.java
@@ -173,9 +173,9 @@ public class MediaChoseActivity extends AppCompatActivity {
             menu.findItem(R.id.menu_photo_count).setEnabled(true);
             menu.findItem(R.id.menu_photo_count).setVisible(true);
             if (chosemode == PickConfig.MODE_MULTIP_PICK) {
-                menu.findItem(R.id.menu_photo_count).setTitle("发送(" + imasgemap.size() + "/" + max_chose_count + ")");
+                menu.findItem(R.id.menu_photo_count).setTitle("选择(" + imasgemap.size() + "/" + max_chose_count + ")");
             } else {
-                menu.findItem(R.id.menu_photo_count).setTitle("发送(1)");
+                menu.findItem(R.id.menu_photo_count).setTitle("选择(1)");
             }
         }
         return super.onCreateOptionsMenu(menu);
